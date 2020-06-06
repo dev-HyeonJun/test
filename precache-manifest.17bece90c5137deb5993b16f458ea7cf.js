d3c0cb606979a6984ddd5fcce269d29f4926f08d
sonteve <sds11609@gmail.com>
2020-06-07 02:44:07 +0900

    Updates

---
+++ precache-manifest.17bece90c5137deb5993b16f458ea7cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5342285076397a79dcc567f7e1c1ca0e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2b50fc868d57b7e64fe3",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c2081f3f40cdee23736",
+    "url": "/test/static/js/2.0cac2437.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.0cac2437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b50fc868d57b7e64fe3",
+    "url": "/test/static/js/main.22b4cb6e.chunk.js"
+  },
+  {
+    "revision": "1f2b0cb618a622904118",
+    "url": "/test/static/js/runtime-main.9bd3e333.js"
+  }
+]);
